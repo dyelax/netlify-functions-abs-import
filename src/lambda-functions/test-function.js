5604fc10f9f047371cf561480cc7b4a3c8f24345
Matt Cooper <me@matt.coop>
2020-06-30 15:58:33 -0700

    Okay some things working now!

--- src/lambda-functions/test-function.js
+++ src/lambda-functions/test-function.js
@@ -6,48 +6,20 @@ import fs from "fs";
 import express from 'express'
 import serverless from 'serverless-http'
 
-const app = express();
-const router = express.Router();
-
-router.get('/', async (req, res) => {
-  console.log(req);
-  console.log('HI');
-  const app = ReactDOMServer.renderToString(<App id={req.query.id} />);
-  res.status(200).send(app);
-
-  // const indexFile = path.resolve('./public/index.html');
-  // // let metas = await getMetas(req);
-  //
-  // console.log(req);
-  // // let metas = getMetas(req);
-  //
-  // fs.readFile(indexFile, 'utf8', (err, indexHtml) => {
-  //   if (err) {
-  //     console.error('Something went wrong:', err);
-  //     return res.status(500).send('Oops, better luck next time!');
-  //   }
-  //
-  //   indexHtml = indexHtml.replace('<div id="root"></div>', `<div id="root">${app}</div>`);
-  //   // indexHtml = setMetas(indexHtml, metas);
-  //
-  //   return res.send(indexHtml);
-  // });
-
-
-
-  // res.writeHead(200, { 'Content-Type': 'text/html' });
-  // res.write('<h1>Hello from Express.js!</h1>');
-  // res.end();
-
-  // res.json({'a': 'wassup'})
-});
-
-app.use('/.netlify/functions/test-function', router);
-
-exports.handler = serverless(app);
-
-// exports.handler = async (event, context) => {
-//   // const indexFile = path.resolve('./build/index.html');
+// const app = express();
+// const router = express.Router();
+//
+// router.get('/', async (req, res) => {
+//   console.log(req);
+//   console.log('HI');
+//   const app = ReactDOMServer.renderToString(<App id={req.query.id} />);
+//   res.status(200).send(app);
+//
+//   // const indexFile = path.resolve('./public/index.html');
+//   // // let metas = await getMetas(req);
+//   //
+//   // console.log(req);
+//   // // let metas = getMetas(req);
 //   //
 //   // fs.readFile(indexFile, 'utf8', (err, indexHtml) => {
 //   //   if (err) {
@@ -55,15 +27,30 @@ exports.handler = serverless(app);
 //   //     return res.status(500).send('Oops, better luck next time!');
 //   //   }
 //   //
-//   //   // indexHtml = indexHtml.replace('<div id="root"></div>', `<div id="root">${app}</div>`);
-//   //   indexHtml = setMetas(indexHtml, metas);
+//   //   indexHtml = indexHtml.replace('<div id="root"></div>', `<div id="root">${app}</div>`);
+//   //   // indexHtml = setMetas(indexHtml, metas);
 //   //
 //   //   return res.send(indexHtml);
 //   // });
-//   console.log(event.queryStringParameters.id);
-//   return {
-//     statusCode: 200,
-//     head: 'asdf',
-//     body: ReactDOMServer.renderToString(<App id={event.queryStringParameters.id} />),
-//   };
-// };
+//
+//
+//
+//   // res.writeHead(200, { 'Content-Type': 'text/html' });
+//   // res.write('<h1>Hello from Express.js!</h1>');
+//   // res.end();
+//
+//   // res.json({'a': 'wassup'})
+// });
+//
+// app.use('/.netlify/functions/test-function', router);
+//
+// exports.handler = serverless(app);
+
+exports.handler = async (event, context) => {
+  console.log(event.queryStringParameters.id);
+  return {
+    statusCode: 200,
+    head: 'asdf',
+    body: ReactDOMServer.renderToString(<App id={event.queryStringParameters.id} />),
+  };
+};
