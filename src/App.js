fff162f86e8f1366ffb2e1b8a089762d8fd2dd32
Matt Cooper <me@matt.coop>
2020-08-25 15:23:48 -0700

    Updates App to set client state in an effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = props => {
-  let content = typeof window === 'undefined' ? 'NOT hydrated' : 'hydrated';
-  return <p>{content}</p>
-}
+  const [onClient, setOnClient] = useState(false)
+  useEffect(() => {
+    setOnClient(true)
+  }, [])
 
+  return (
+    onClient
+      ? <p>Hydrated!</p>
+      : <p>Not Hydrated :(</p>
+  )
+}
 export default App;
